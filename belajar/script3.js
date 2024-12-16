77e4ad6b502ad08db5fa4749932da2ee7e7d54d7
indra <indraprastio9412@gmail.com>
2024-12-16 18:24:05 +0530

    Add files via upload

---
+++ belajar/script3.js
@@ -0,0 +1,16 @@
+umur = 40;
+
+satus =""
+if(umur>=41) {
+    satus = "orang yua"
+} else if (umur> 21) {
+    satus = "dewasa"
+} else if (umur> 12) {
+    satus = "remaja"
+} else if (umur>5) {
+    satus = "anak-anak"
+} else{
+    satus = "balita"
+}
+
+document.write(satus)
