2402842bb83836dc17b56b330387a67723aafa15
indra <indraprastio9412@gmail.com>
2024-12-27 21:28:42 +0800

    Add files via upload

---
+++ python  project/my function.py
@@ -0,0 +1,38 @@
+nama = "umar"
+print("nama:", nama)
+
+#
+
+def MyFunction():
+    nama = "umar"
+    alamat = "jakarta"
+    print("nama:", nama)
+    print("alamat:",alamat)
+MyFunction()
+
+
+
+def MyFunctionwithParameter(nama,alamat):
+    print("nama:", nama)
+    print("alamat:", alamat)
+MyFunctionwithParameter("indra","setiling")
+MyFunctionwithParameter("robi","setiling")
+
+nama = "ibuku"
+MyFunctionwithParameter(nama,"setiling")
+
+
+print("pogram selesai")
+
+
+
+
+
+
+# buat fungsi untuk menghitung nilai faktorial
+
+def Faktorial(x):
+#code
+    hasil = 0
+    return hasil
+print(Faktorial(5))
